f9eece328ad292700920dd4cc6b19d7d0d22d73c
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2021-07-23 09:53:04 -0500

    Version 1.2

--- Webhook/connection.py
+++ Webhook/connection.py
@@ -1,4 +1,5 @@
-import http.client, Webhook
+import http.client
+import Webhook
 
 def id():
     conn = http.client.HTTPSConnection('api.pipedream.com')
@@ -16,27 +17,24 @@ def get():
     data = res.read().decode("utf-8")
     return data
 
-def delete():
+def delete(dept):
     dept_id = {}
     dept_id = Webhook.current_id
 
-    if(len(dept_id) != 0):
-        end = dept_id[len(dept_id)-1]
-        start = dept_id[0]
-        print(end, start)
+    if(dept == "All" and len(dept_id) != 0):
+        end = dept_id[0]
+        start = dept_id[len(dept_id)-1]
         conn = http.client.HTTPSConnection('api.pipedream.com')
         conn.request("DELETE", '/v1/sources/<webhook>/events?start_id=%s&end_id=%s' % (start, end), '', {
         'Authorization': 'Bearer <api_key>'})
         conn.getresponse() 
-
-def delete_current():
-    dept_id = {}
-    dept_id = Webhook.current_id
-
-    if(len(dept_id) != 0):
+        dept_id.clear()
+        
+    elif(dept != "All" and len(dept_id) != 0):
         for x in range (len(dept_id)):
             end = dept_id[x]
             conn = http.client.HTTPSConnection('api.pipedream.com')
             conn.request("DELETE", '/v1/sources/<webhook>/events?start_id=%s&end_id=%s' % (end, end), '', {
-            'Authorization': 'Bearer <api_key'})
+            'Authorization': 'Bearer <api_key>'})
             conn.getresponse()
+        dept_id.clear()
