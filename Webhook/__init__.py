8034b1c3964e97f8069ca7e43eb135a1dcf244b7
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2021-07-27 10:00:47 -0500

    Version 1.3.1

--- Webhook/__init__.py
+++ Webhook/__init__.py
@@ -1,7 +1,8 @@
-#https://timothybramlett.com/How_to_create_a_Python_Package_with___init__py.html
+##https://timothybramlett.com/How_to_create_a_Python_Package_with___init__py.html
 from .connection import *
 from .requests import *
 from .interface import *
 from .dictionaries import *
 
-__version__ = "1.2.1"
+__version__ = "1.3.1"
+__title__ = "Clive-1.3.1"
