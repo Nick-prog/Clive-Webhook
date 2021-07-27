aa468b582ea6416b998d0cbc7881763b7ca2d23b
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2021-07-27 09:57:40 -0500

    Version 1.3.1

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import timeit
 class Enrollment:
 
     def display(self):
+        start = timeit.default_timer()
         app = Webhook.QtWidgets.QApplication(sys.argv)
         Dialog = Webhook.QtWidgets.QDialog()
         ui = Webhook.Ui_Dialog()
         ui.setupUi(Dialog)
         Dialog.show()
+        stop = timeit.default_timer()
+        print("Time: %s" %(stop - start))
         sys.exit(app.exec_())
 
 E = Enrollment()
