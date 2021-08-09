3e70d3481998678e4714c5ded3e17837388a4b35
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2021-08-09 08:38:10 -0500

    Version 1.4.1

--- Webhook/connection.py
+++ Webhook/connection.py
@@ -3,7 +3,7 @@ import Webhook
 
 def id():
     conn = http.client.HTTPSConnection('api.pipedream.com')
-    conn.request("GET", '/v1/sources/<webhook>/event_summaries', '', {
+    conn.request("GET", '/v1/sources/<webhook_1>/event_summaries', '', {
         'Authorization': 'Bearer <api_key>'})
     res = conn.getresponse()
     data = res.read().decode("utf-8")
@@ -11,7 +11,7 @@ def id():
 
 def get():
     conn = http.client.HTTPSConnection('api.pipedream.com')
-    conn.request("GET", '/v1/sources/<webhook>/event_summaries?expand=event', '', {
+    conn.request("GET", '/v1/sources/<webhook_1>/event_summaries?expand=event', '', {
         'Authorization': 'Bearer <api_key>'})
     res = conn.getresponse()
     data = res.read().decode("utf-8")
@@ -25,7 +25,7 @@ def delete(dept):
         end = dept_id[0]
         start = dept_id[len(dept_id)-1]
         conn = http.client.HTTPSConnection('api.pipedream.com')
-        conn.request("DELETE", '/v1/sources/<webhook>/events?start_id=%s&end_id=%s' % (start, end), '', {
+        conn.request("DELETE", '/v1/sources/<webhook_1>/events?start_id=%s&end_id=%s' % (start, end), '', {
         'Authorization': 'Bearer <api_key>'})
         conn.getresponse() 
         dept_id.clear()
@@ -34,7 +34,36 @@ def delete(dept):
         for x in range (len(dept_id)):
             end = dept_id[x]
             conn = http.client.HTTPSConnection('api.pipedream.com')
-            conn.request("DELETE", '/v1/sources/<webhook>/events?start_id=%s&end_id=%s' % (end, end), '', {
+            conn.request("DELETE", '/v1/sources/<webhook_1>/events?start_id=%s&end_id=%s' % (end, end), '', {
             'Authorization': 'Bearer <api_key>'})
             conn.getresponse()
         dept_id.clear()
+
+def id_Fee():
+    conn = http.client.HTTPSConnection('api.pipedream.com')
+    conn.request("GET", '/v1/sources/<webhook_2>/event_summaries', '', {
+        'Authorization': 'Bearer <api_key>'})
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    return data #Returns id metadata for every entry
+
+def get_Fee():
+    conn = http.client.HTTPSConnection('api.pipedream.com')
+    conn.request("GET", '/v1/sources/<webhook_2>/event_summaries?expand=event', '', {
+        'Authorization': 'Bearer <api_key>'})
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    return data #Returns JSON Snipper metadata for every entry
+
+def delete_Fee():
+    id = {}
+    id = Webhook.current_id #Grab the currently stored ids of the entries just pulled
+
+    if(len(id) != 0): #If something is there to pull, delete everything pulled between the first and last entry
+        end = id[0]
+        start = id[len(id)-1]
+        conn = http.client.HTTPSConnection('api.pipedream.com')
+        conn.request("DELETE", '/v1/sources/<webhook_2>/events?start_id=%s&end_id=%s' % (start, end), '', {
+        'Authorization': 'Bearer <api_key>'})
+        conn.getresponse()
+        id.clear()
