c1e3bfec54e9e5eff8bf17e41d475300d09b9429
Nick-prog <Nickolasrodriguez98@gmail.com>
2024-06-26 12:02:54 -0500

    version 2.0 updated

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import Webhook
 import sys
 import tkinter as tk
+import pymsgbox
 
 try:
-    total1 = Webhook.total(method=1)
+    upload_total = Webhook.total(method=1)
     Webhook.uploaded_documents()
-    tk.messagebox.showerror("%s uploaded documents pulled." %(total1), "Complete!")
 
-    total2 = Webhook.total(method=2)
+    fee_total = Webhook.total(method=2)
     Webhook.fee_documents()
-    tk.messagebox.showerror("%s fee documents pulled." %(total2), "Complete!")
-except ValueError:
-    tk.messagebox.showerror("ValueError encountered at entry %s. %s" %(len(Webhook.IDs), sys.exc_info()[1]), "Warning!")
-    pass
-except TypeError:
-    tk.messagebox.showerror("TypeError encountered at entry %s. %s" %(len(Webhook.IDs), sys.exc_info()[1]), "Warning!")
-    pass
-except:
+
+    pymsgbox.alert(f"{upload_total+fee_total} documents pulled.\n{upload_total} upload and {fee_total} fee.", "Complete!")
+except BaseException as b:
     tk.messagebox.showerror("Something terrible occurred at entry %s. %s, %s" %(len(Webhook.IDs), sys.exc_info()[0], sys.exc_info()[1]), "Warning!")
-    pass
