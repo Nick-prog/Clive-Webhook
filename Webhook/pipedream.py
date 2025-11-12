3dd23dd3197d1a07bad9dedbe52b5a507c36ffc7
Nick-prog <Nickolasrodriguez98@gmail.com>
2025-11-12 14:36:48 -0600

    Shim: alias Webhook.pipedream to src.webhook.pipedream module

--- Webhook/pipedream.py
+++ Webhook/pipedream.py
@@ -1,2 +1,4 @@
-# Shim to keep backward compatibility after moving code to src/
-from src.webhook.pipedream import *  # noqa: F401,F403
+import importlib, sys
+_mod = importlib.import_module('src.webhook.pipedream')
+sys.modules[__name__] = _mod
+
